461337dc3445b14a1b43dcf42dcc9c4b807b9877
Богдан Снигирь <bogdansnigir@MacBook-Air-Bogdan.local>
2023-09-28 14:59:26 +0600

    Решил 4 задачи

---
+++ Homework_1.cs
@@ -0,0 +1,77 @@
+﻿//Напишите программу, которая на вход принимает два числа и 
+//выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Input a number one: ");
+// int numone= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input a number two: ");
+// int numtwo= Convert.ToInt32(Console.ReadLine());
+
+// if (numone > numtwo)
+// {
+//    Console.WriteLine($"{numone} is bigger then {numtwo}");
+// }
+// else{
+//    Console.WriteLine($"{numtwo} is bigger then {numone}");
+// }
+
+
+
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Input a number one: ");
+// int n1= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input a number two: ");
+// int n2= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input a number three: ");
+// int n3= Convert.ToInt32(Console.ReadLine());
+
+// if (n1>n2){
+//     if (n1>n3){
+//         Console.WriteLine($"{n1} is the biggest number");
+//     }
+//     else if (n3>n2) {
+//         Console.WriteLine($"{n3} is the biggest number");
+//     }
+// }
+// else {
+//     if (n2>n3){
+//         Console.WriteLine($"{n2} is the biggest number");
+//     }
+//     else if (n3>n2) {
+//         Console.WriteLine($"{n3} is the biggest number");
+//     }
+// }
+
+
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Input a number: ");
+// int num= Convert.ToInt32(Console.ReadLine());
+
+// int result = num % 2;
+
+// if (result == 1){
+//     Console.WriteLine($"{num} is odd number");
+// }
+// else {
+//     Console.WriteLine($"{num} is even number");
+// }
+
+
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Input a number: ");
+// int n= Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// while(count<=n){
+//     Console.WriteLine($"{count}");
+//     count = count + 2;
+// }
+//Примечание, в задаче указано "до N", поэтому я посчитал, что если N - четное число, то пусть программа его тоже включает. 
+//Для этого в цикле while я и добавил равно. Если же N не входит в этот промежуток, тогда равно можно просто убрать и схема будет работать.
